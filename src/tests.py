4e42b2ddeb28add5f246121d75ae64b07420aa9d
linaguz020 <linaguz020@gmail.com>
2025-04-22 20:30:29 -0700

    added tests for size and search

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import LinkedList
 
+# This class tests Linked List functions
 class TestLinkedList(unittest.TestCase):
     def create_sample_list(self):
         new_list = LinkedList.LinkedList()
@@ -24,6 +25,26 @@ class TestLinkedList(unittest.TestCase):
     def test_add_to_filled_list(self):
         new_list = self.create_sample_list()
         self.assertEqual(new_list.printList(), [12, 14, 13, 20])
+
+    def test_empty_list_size(self):
+        new_list = LinkedList.LinkedList()
+        self.assertEqual(new_list.size(), 0)
+
+    def test_filled_list_size(self):
+        new_list = self.create_sample_list()
+        self.assertEqual(new_list.size(), 4)
+
+    def test_search_empty_list(self):
+        empty_list = LinkedList.LinkedList()
+        self.assertEqual(empty_list.search(12), False)
+
+    def test_search_known_val_filled_list(self):
+        new_list = self.create_sample_list()
+        self.assertEqual(new_list.search(12), True)
+    
+    def test_search_missing_val_filled_list(self):
+        new_list = self.create_sample_list()
+        self.assertEqual(new_list.search(-1), False)
 
 
 if __name__ == '__main__':
